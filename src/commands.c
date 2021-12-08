e15363f000817b3f4cfb785869471a0012c1825f
tegenton <22758764+tegenton@users.noreply.github.com>
2021-12-08 13:53:07 -0500

    usage field in cmd struct

--- src/commands.c
+++ src/commands.c
@@ -2,17 +2,19 @@
 #include <string.h>
 #include "commands.h"
 
-#define COMMAND_COUNT 1
+#define COMMAND_COUNT 2
 
 struct cmd {
     const char *name;
     int (*func)(int, const char**);
+    const char **usage;
 };
 
 int help(int argc, const char **argv);
 
 static struct cmd commands[COMMAND_COUNT] = {
-        {"help", help},
+        {"help", help, (const char*[]){NULL}},
+        {"version", version, (const char*[]){NULL}},
 };
 
 int execute(int argc, const char **argv) {
@@ -25,11 +27,13 @@ int execute(int argc, const char **argv) {
 }
 
 int help(int argc, const char **argv) {
-    printf("You asked for help with:");
-    for (int i = 0; i < argc; i++) {
-        printf(" %s", argv[0]);
-        argv++;
+    printf("wmc usage:\n");
+    for (int i = 0, j = 0; i < COMMAND_COUNT; i++, j = 0) {
+        printf("%s", commands[i].name);
+        while (commands[i].usage[j] != NULL) {
+            printf(" %s", commands[i].usage[j++]);
+        }
+        printf("\n");
     }
-    printf("\n");
     return 0;
 }
